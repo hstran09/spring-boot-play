e7e65b025bee69a5737942f4d8be8ee9b03c7a18
Son Tran <hstran09@gmail.com>
2021-07-01 01:55:15 +0700

    init

---
+++ src/test/java/ind/hstran09/junit/JunitApplicationTests.java
@@ -0,0 +1,13 @@
+package ind.hstran09.junit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
